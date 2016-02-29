1f49c98226d8f93d802c3c34954f11e1e15df948
EpicWin332 <gamzatovmagomed@mail.ru>
2016-02-29 23:48:15 +0300

    second activity

--- app/src/main/java/com/magomed/gamzatov/universalmarket/MainActivity.java
+++ app/src/main/java/com/magomed/gamzatov/universalmarket/MainActivity.java
@@ -1,5 +1,6 @@
 package com.magomed.gamzatov.universalmarket;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +9,12 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,13 +24,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        ListView listViewCategory = (ListView) findViewById(R.id.listViewCategory);
+        String[] list = {"Все", "Категория 1", "Категория 2", "Категория 3"};
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, list);
+        listViewCategory.setAdapter(arrayAdapter);
+        listViewCategory.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(MainActivity.this, ItemsList.class);
+                startActivity(intent);
             }
         });
     }
